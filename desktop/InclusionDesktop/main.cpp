f8d35cbe9aaa6e4bc60eaf53f76e58cbb7f8120d
Yifei Liu <37576347+luojuee@users.noreply.github.com>
2021-03-10 14:53:47 +0100

    login UI

--- desktop/InclusionDesktop/main.cpp
+++ desktop/InclusionDesktop/main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
+#include "form.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    Form login;
+    login.show();
+    //MainWindow w;
+    //w.show();
     return a.exec();
 }
