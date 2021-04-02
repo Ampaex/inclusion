3974590c74c7e393c7e131bba6c7bec2cc619622
Antonio <antoniovavazquez@gmail.com>
2021-04-02 17:52:11 +0200

    Añadida funcionalidad del login y cambio a la ventana principal

--- desktop/InclusionDesktop/form.h
+++ desktop/InclusionDesktop/form.h
@@ -2,6 +2,7 @@
 #define FORM_H
 
 #include <QWidget>
+#include "mainwindow.h"
 
 namespace Ui {
 class Form;
@@ -13,10 +14,16 @@ class Form : public QWidget
 
 public:
     explicit Form(QWidget *parent = nullptr);
+    void onClick();
     ~Form();
+
+private slots:
+    void on_loginButton_clicked();
 
 private:
     Ui::Form *ui;
+    QString username;
+    MainWindow mainwindow;
 };
 
 #endif // FORM_H
