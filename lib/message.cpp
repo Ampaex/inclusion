fc84dabc60a7c2e266b203612ee9f450cf2f41b0
JhohanLo <johan.lopez19@hotmail.com>
2021-04-04 16:51:41 +0200

    Clase message
    se desarrolla clase message con su get y set

--- lib/message.cpp
+++ lib/message.cpp
@@ -1,1 +1,18 @@
+#include "message.h"
+
+using namespace std;
+
+message::message( char m)
+{
+  messages = m;
+
+}
+
+void message:: setMessage(char m){
+    messages = m;
+}
+
+char message:: getMessage(){
+     return messages;
+}
 
--- lib/message.h
+++ lib/message.h
@@ -1,1 +1,18 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
 
+
+class message
+{
+    private:
+    
+    char messages;   // Private attribute
+
+    public:
+    message(char m);
+    void setMessage(char m);
+    char getMessage();
+
+    };
+
+#endif // MESSAGE_H
