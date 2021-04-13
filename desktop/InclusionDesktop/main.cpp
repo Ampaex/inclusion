da55736f1b9e4e4bcf0f99a1d732413628e8b0eb
Antonio <antoniovavazquez@gmail.com>
2021-04-13 22:18:37 +0200

    Ahora la ventana de login aparece centrada

--- desktop/InclusionDesktop/main.cpp
+++ desktop/InclusionDesktop/main.cpp
@@ -2,11 +2,13 @@
 #include "form.h"
 
 #include <QApplication>
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Form login;
+    login.move(QApplication::desktop()->screen()->rect().center() - login.rect().center());
     login.show();
 
     return a.exec();
